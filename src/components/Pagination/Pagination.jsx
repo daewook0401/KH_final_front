8a9653b11f3b10ca5755eadcd42e23d12670ad14
shakeit <134345291+kyung9687@users.noreply.github.com>
2025-06-27 14:34:55 +0900

    페이지네이션 css

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,20 +1,43 @@
 import PaginationLib from "react-js-pagination";
 
-// npm install react-js-pagination
 const Pagination = ({ currentPage, setCurrentPage, pageInfo }) => {
   const { boardNoPerPage, totalBoardNo, pageSize } = pageInfo;
+  const arrowClass =
+    "w-10 h-10 flex items-center justify-center border border-orange-300 rounded-xl text-sm font-semibold text-orange-400 hover:bg-orange-200 transition duration-150";
+
+  const totalPages = Math.ceil(totalBoardNo / boardNoPerPage);
+
   return (
-    <div>
+    <div className="flex justify-center mt-6">
       <PaginationLib
         activePage={currentPage}
         itemsCountPerPage={boardNoPerPage}
         totalItemsCount={totalBoardNo}
         pageRangeDisplayed={pageSize}
-        prevPageText={"<"}
-        nextPageText={">"}
-        onChange={(currentPage) => setCurrentPage(currentPage)}
+        onChange={setCurrentPage}
+        firstPageText={
+          currentPage !== 1 && <span className={arrowClass}>{"<<"}</span>
+        }
+        prevPageText={
+          currentPage !== 1 && <span className={arrowClass}>{"<"}</span>
+        }
+        nextPageText={
+          currentPage !== totalPages && (
+            <span className={arrowClass}>{">"}</span>
+          )
+        }
+        lastPageText={
+          currentPage !== totalPages && (
+            <span className={arrowClass}>{">>"}</span>
+          )
+        }
+        itemClass="inline-block mx-1"
+        linkClass="w-8 h-8 flex items-center justify-center text-sm text-gray-700 hover:bg-gray-200 rounded-full"
+        activeLinkClass="bg-orange-500 text-white"
+        disabledClass="opacity-50 cursor-not-allowed"
       />
     </div>
   );
 };
+
 export default Pagination;
