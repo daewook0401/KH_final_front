8e37316cb569f58f1e9388f183dce818bf056d88
daewook0401 <daewook0401@naver.com>
2025-07-07 17:40:24 +0900

    수정

--- src/provider/AuthContext.jsx
+++ src/provider/AuthContext.jsx
@@ -50,7 +50,7 @@ export const AuthProvider = ({ children }) => {
     [auth]
   );
   useEffect(() => {
-    if (!!!sessionStorage.getItem("loginInfo") && JSON.parse(sessionStorage.getItem("loginInfo")).isActive === "N"){
+    if (sessionStorage.getItem("loginInfo") !== null && JSON.parse(sessionStorage.getItem("loginInfo")).isActive === "N"){
       alert("비활성 사용자입니다. 로그아웃 됩니다.");
       setAuth({
         loginInfo: null,
